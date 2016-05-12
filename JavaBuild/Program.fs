7475f3ece901c9d6b55e61a12b3e3ccfc839ad56
amoretspero <amoretspero@gmail.com>
2016-05-13 02:54:11 +0900

    Initial Commit.

---
+++ JavaBuild/Program.fs
@@ -0,0 +1,109 @@
+﻿open System
+open System.Collections
+open System.Collections.Generic
+open System.IO
+open System.Text
+open System.Management
+open Newtonsoft.Json
+
+type JavaBuild(files : string [], mains : string [], CompileOptions : Dictionary<string, string>) =
+    let _files = files
+    let _mains = mains
+    let _CompileOptions = CompileOptions
+
+    member jb.files = _files
+    member jb.mains = _mains
+    member jb.CompileOptions = _CompileOptions
+
+    member jb.printFiles() =
+        for file in _files do
+            printfn "Input File : %s" file
+    member jb.printMains() =
+        for main in _mains do
+            printfn "Input File with main : %s" main
+    member jb.printCompileOptions() =
+        for co in _CompileOptions do
+            if (co.Value = "") then
+                printfn "Option Name : %s, Option Value : (none)" co.Key
+            else
+                printfn "Option Name : %s, Option Value : %s" co.Key co.Value
+    member jb.GetFormalOption(compileOption : string) =
+        match compileOption with
+        | "ClassPath" -> "-classpath"
+        | "CharacterEncoding" -> "-encoding"
+        | "TerminateOnWarning" -> 
+            let optionValue = ref ""
+            _CompileOptions.TryGetValue(compileOption, optionValue) |> ignore
+            if (optionValue.Value = "True") then
+                "-Werror"
+            else
+                ""
+        | _ -> ""
+    member jb.GetFormalOptionValue(compilerOption : string, optionValue : string) =
+        match compilerOption with
+        | "ClassPath" -> optionValue
+        | "CharacterEncoding" -> optionValue
+        | "TerminateOnWarning" -> ""
+        | _ -> ""
+    member jb.CompilerOptionsToString() =
+        let mutable res = ""
+        for co in _CompileOptions do
+            if (co.Value <> "") then
+                res <- res + jb.GetFormalOption(co.Key) + " " + jb.GetFormalOptionValue(co.Key, co.Value) + " "
+        res
+            
+    
+
+let buildFile = JsonConvert.DeserializeObject<JavaBuild>(File.ReadAllText("./build.json"))
+printfn "Generated buildFile. files : %d, mains : %d, CompileOptions: %d" buildFile.files.Length buildFile.mains.Length buildFile.CompileOptions.Count
+buildFile.printFiles()
+buildFile.printMains()
+buildFile.printCompileOptions()
+
+let compileSuccess = ref ([] : string list)
+let compileFail = ref ([] : string list)
+
+for file in buildFile.files do
+    printfn "=================================================="
+    printfn "Now compiling '%s'..." (file+".java")
+    printfn "\n"
+    let proc = new System.Diagnostics.Process()
+    let procStartInfo = new System.Diagnostics.ProcessStartInfo()
+    printfn "Command to be executed : %s" ("javac "+file+".java"+" "+(buildFile.CompilerOptionsToString()))
+    procStartInfo.WindowStyle <- System.Diagnostics.ProcessWindowStyle.Hidden
+    procStartInfo.FileName <- "javac "
+    procStartInfo.Arguments <- buildFile.files.[0] + ".java" + " " + (buildFile.CompilerOptionsToString())
+    procStartInfo.RedirectStandardOutput <- true
+    procStartInfo.RedirectStandardError <- true
+    procStartInfo.UseShellExecute <- false
+    proc.StartInfo <- procStartInfo
+    proc.Start() |> ignore
+    proc.WaitForExit()
+    let procResult = proc.StandardOutput.ReadToEnd()
+    let procError = proc.StandardError.ReadToEnd()
+    if (procError.Length = 0 && procResult.Length = 0) then
+        compileSuccess.Value <- List.append compileSuccess.Value [file]
+        printfn "Execution Result(output) : \nSuccess."
+        printfn "\nFinished compiling '%s'. Comilation result : SUCCESS" (file+".java")
+    else if (procError.Length = 0 && procResult.Length > 0) then
+        compileSuccess.Value <- List.append compileSuccess.Value [file]
+        printfn "Execution Result(output) : \n%s" procResult
+        printfn "Finished compiling '%s'. Comilation result : SUCCESS" (file+".java")
+    else
+        compileFail.Value <- List.append compileSuccess.Value [file]
+        printfn "Execution Result(error) : \n%s" procError
+        printfn "Finished compiling '%s'. Comilation result : FAIL" (file+".java")
+    printfn "=================================================="
+
+printfn "=================================================="
+printfn "Build Result :\n"
+printfn "Number of requested compilations: %d\n" buildFile.files.Length
+printfn "Compile SUCCESS: \n"
+for file in compileSuccess.Value do
+    printfn "     %s" file
+printfn "\nCompile FAIL   : \n"
+for file in compileFail.Value do
+    printfn "     %s" file
+printfn "\nSUCCESS: %d files.\nFAIL: %d files.\n" compileSuccess.Value.Length compileFail.Value.Length
+printfn "END of BUILD."
+printfn "=================================================="
---
+++ JavaBuild/README.md
@@ -0,0 +1,51 @@
+﻿# JavaBuild  
+  
+## Usage  
+To be added.  
+  
+## Build execution  
+  
+### Requirements  
+.NET Framework 4.6 or higher  
+F# 4.0 or higher.  
+fsc(F# compiler)  
+
+### Build Executable - Visual Studio (2015 or higher)  
+Run the solution!  
+  
+### Build Executable - Command line  
+Go to source folder and type below.  
+<code>fsc -o:JavaBuild.exe --target:exe --staticlink:Newtonsoft.Json -r:Newtonsoft.Json -r:Microsoft.Management.Infrastructure -r:System.Management.Automation Program.fs</code>  
+  
+## Notice  
+  
+### JSON parsing    
+JSON parsing uses <b>Newtonsoft.Json</b> library.  
+They can be found at <link rel="http://www.newtonsoft.com/json"/>  
+License about this library can be found in above link.  
+
+## License  
+This program is open source under MIT license.  
+  
+Copyright (c) 2016 Jiung Hahm
+  
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+  
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+  
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
